3e8ec1fbddc27035d807a63d63a8418e9270614b
Edward Bock <me@edwardbock.de>
2013-04-11 16:27:16 +0200

    Magic
    UpdateContainer, addbox, searchBox, toggle optimiert

--- js/grid2.0.js
+++ js/grid2.0.js
@@ -1,189 +1,34 @@
 
 $(function() {
-	// init Methode
-	var ID;
-	function init() {
-		ID = 43;
-		loadGrid();
-	}
-	$(document).ready(init);
-	// Elemente
-	$stateDisplay = $(".state-display");
 	// Warnung beim verlassen ohne Speichern
+	/*
 	var saved = true;
 	window.onbeforeunload = function(){
 		if(!saved) return "Es wurden noch nicht alle Änderungen gespeichert!";
 	}
-	// Handler für die Toolbar Buttons
-	$("#toolbar").on("click","button",function(e){
-		$this = $(this);
-		switch($this.attr("role")){
-			case "load":
-				loadGrid();
-				console.log("Container laden");
-				break;
-			case "save":
-				saveGrid();
-				console.log("Container speichern");
-				break;
-			case "add_container":
-				console.log("Container hinzufügen");
-				break;
-			case "add_box":
-				console.log("Box hinzufügen");
-				break;
-			case "hide_boxes":
-				toggleBoxes();
-				console.log("Boxen ausblenden");
-				break;
-			default:
-				console.log("Role unbekannt: "+$this.attr("role"));
-				break;
-		}
-	});
-	// container funktionen
-	var GRID_SORTABLE = "#grid";
-	var $grid = $(GRID_SORTABLE).sortable({
-		handle: ".c-sort-handle",
-		//axis: "y",
-		//cursor: "row-resize",
-		items:".container",
-		placeholder: "c-sort-placeholder",
-		start: function( event, ui ){
-			//$(".box").slideUp(100);
-			console.log("started sorting container");
-		},
-		stop: function(event, ui){
-			//$(".box").slideDown(100);
-		},
-		update: function( event, ui ){
-			params = [ID, ui.item.data("id"), ui.item.index()];
-			sendAjax("moveContainer",params,
-					function(data){
-						if(data.result != true){
-							console.log("Fehler! Element muss an originalposition zurück");
-						}
-					}, function(jqXHR, textStatus, error){
-						console.log(error);
-					});
-		}
-	});
-	var CONTAINER_DROP_AREA_CLASS = "container-drop-area";
-	$("#container-dragger").draggable({ 
-		helper: "clone", 
-		zIndex: 99, 
-		//connectToSortable: GRID_SORTABLE,
-		start: function(event, ui){
-			$grid.children().before("<div class='"+CONTAINER_DROP_AREA_CLASS+"'>Drop Area</div>");
-			$grid.append("<div class='"+CONTAINER_DROP_AREA_CLASS+"'>Drop Area</div>");
-			$grid.children("."+CONTAINER_DROP_AREA_CLASS).droppable({ 
-				accept: ".new-container",
-				drop: function( event, ui ) {
-					containerType =  $("select[name=container-type]").val();
-					$temp = $.tmpl( "containerTemplate", [{
-											"type": containerType, 
-											"id" : "new",
-											"prolog": "prolog",
-											"epilog": "epilog" }] ).insertBefore( $(this) );
-					
-					$grid.children().remove("."+CONTAINER_DROP_AREA_CLASS);
-					params = [ID, containerType, $temp.index()];
-					sendAjax("addContainer",params,function(data){
-						console.log(data.result.id);
-						$temp.data("id", data.result.id);
-						$slots_wrapper = $temp.find(".slots-wrapper");
-						$.each( data.result.slots, function(index,value){
-							console.log(index+" "+value);
-							$.tmpl( "slotTemplate", [{ 
-											"id" : value }] ).appendTo( $slots_wrapper );
-						});
-					}, function(jqXHR, textStatus, error){
-						console.log(error);
-					});
-				}
-			});
-		},
-		stop: function( event, ui ){
-			$grid.children().remove("."+CONTAINER_DROP_AREA_CLASS);
-		}
-	});
-	$grid.on("click",".container > .tools > .c-trash", function(e){
-		$this = $(this);
-		$container = $this.parents(".container");
-		params = [ID, $container.data("id")];
-		console.log($container.data("id"));
-		sendAjax("deleteContainer",params,function(data){
-			if(data.result != true){
-				alert("fehler beim Löschen!");
-				return;
-			}
-			$container.slideUp(300,function(){
-				$container.remove();
-			});
-		}, function(jqXHR, textStatus, error){
-			console.log(textStatus);
-		});
-	});
-	$grid.on("click",".container > .tools > .c-edit", function(e){
-		$this = $(this);
-		$container = $this.parents(".container");
-		params = {
-					id:$container.data("id"), 
-					title: $container.find(".c-title").text(),
-					titleurl: "url",
-					type: $container.find(".c-title").text(),
-					prolog: "test",
-					epilog: "test",
-					readmore: "readmore",
-					readmoreurl: "url"
-				};
-		$newContainer = $.tmpl( "containerEditorTemplate", params ).insertAfter( $container );
-		$newContainer.find(".slots-wrapper").append($container.find(".slots-wrapper"));
-		$container.remove();
-	});
-	$grid.on("click",".container > .tools > .c-ok", function(e){
-		$this = $(this);
-		$container = $this.parents(".container");
-		params = {
-					id:$container.data("id"), 
-					title: $container.find("#f-c-title").val(),
-					titleurl: $container.find("#f-c-titleurl").val(),
-					type: $container.data("type"),
-					prolog: $container.find("#f-c-prolog").val(),
-					epilog: $container.find("#f-c-epilog").val(),
-					readmore: $container.find("#f-c-readmore").val(),
-					readmoreurl: $container.find("#f-c-readmoreurl").val()
-				};
-		$newContainer = $.tmpl( "containerTemplate", params ).prependTo( $grid );
-		$newContainer.find(".slots-wrapper").append($container.find(".slots-wrapper"));
-		$container.remove();
-	});
-	// slot funktionen
-	
-	
-	// Box funktionen
-	$(".slot").sortable({
-		items: ".box",
-		connectWith: ".slot",
-		placeholder: "b-sort-placeholder",
-		forcePlaceholderSize: true,
-		helper: function(event, element){
-			return $("<div class='b-sort-helper'></div>");
-		},
-		start: function(e, ui){
-			ui.placeholder.height(ui.item.height());
-		},
-		cursorAt: { left: 30, top:30 }
-	});
-	// Grid AJAX functions
+	*/
+	// ----------------
+	// init Methode
+	// ---------------
+	var ID;
+	function init() {
+		ID = 42;
+		loadGrid();
+	}
+	$(document).ready(init);
+	// --------------------
+	// Grid laden
+	// -------------------
 	function loadGrid(){	
 		json = {};
 		json["method"] = "loadGrid";
 		json["params"] = [ID];
 		sendAjax(
 			"loadGrid", 
-			[ID], 
+			[ID],
 			function(data){
+				console.log("loadGrid:");
+				console.log(data);
 				$grid.empty();
 				var result = data.result;
 				if(result.isDraft == true){
@@ -192,46 +37,336 @@ $(function() {
 					$stateDisplay.text("Aktuell!");
 				}
 				var container_arr = result.container;
-				$.tmpl( "containerTemplate", container_arr ).appendTo( $grid );
+				buildContainer(container_arr).appendTo( $grid );
 				console.log(container_arr);
-			}, 
-			function(jqXHR, textStatus, error){
-				console.log(error);
+				refreshBoxSortable()
 			}
 		);
 	}
-	function saveGrid(){
-		json = {};
-		json["method"] = "saveGrid";
-		params = {};
-		// container auslesen
-			// solots auslesen
-				// Boxen auslesen
-		$.post(
-			SERVER,
-			{
-				"method": "saveGrid",
-				"param" : JSON.stringify(param)
-			},function(data){
-				console.log(data);
-			}, 
-			"json");
+		
+	// ---------------------------
+	// Allgemeine Elemente und konstanten
+	// --------------------------
+	var $stateDisplay = $(".state-display");
+	var CSS_GRID_ID = "#grid";
+	var $grid = $(CSS_GRID_ID);
+	
+	var CONTAINER_DROP_AREA_CLASS = "container-drop-area";
+	var CSS_CONTAINER_DROP_AREA_CLASS = ".container-drop-area";
+	
+	var BOX_DROP_AREA_CLASS = "box-drop-area";
+	var CSS_BOX_DROP_AREA_CLASS = ".box-drop-area";
+	
+	var $toolbar = $("#toolbar");
+	var $gridTools = $(".grid-tools");
+	var $toolContainer = $gridTools.children(".g-container");
+	var $toolBox = $gridTools.children(".g-box");
+	
+	var $toolBoxList = $(".g-box .box-list");
+	
+	// ---------------------------------
+	// Handler für die Werkzeuge
+	// ---------------------------------
+	$toolbar.on("click","button",function(e){
+		$this = $(this);
+		switch($this.attr("role")){
+			case "load":
+				loadGrid();
+				break;
+			case "save":
+				saveGrid();
+				break;
+			case "publish":
+				console.log("Veröffentlichen");
+			case "add_container":
+				$gridTools.children().hide();
+				$toolContainer.show();
+				break;
+			case "add_box":
+				showBoxTools();
+				break;
+			case "hide_boxes":
+				toggleBoxes();
+				break;
+			default:
+				console.log("Role unbekannt: "+$this.attr("role"));
+				break;
+		}
+	});
+	// ----------------------------
+	// container funktionen
+	// --------------------------
+	
+	$grid.sortable({
+		handle: ".c-sort-handle",
+		//axis: "y",
+		//cursor: "row-resize",
+		items:".container",
+		placeholder: "c-sort-placeholder",
+		start: function( event, ui ){
+			//$(".box").slideUp(100);
+		},
+		stop: function(event, ui){
+			//$(".box").slideDown(100);
+		},
+		update: function( event, ui ){
+			params = [ID, ui.item.data("id"), ui.item.index()];
+			sendAjax("moveContainer",params,
+			function(data){
+				if(data.result != true){
+					console.log("Fehler! Element muss an originalposition zurück");
+				}
+			});
+		}
+	});
+	$("#container-dragger").draggable({ 
+		helper: "clone", 
+		zIndex: 99, 
+		//connectToSortable: GRID_SORTABLE,
+		start: function(event, ui){
+			$grid.children().before("<div class='"+CONTAINER_DROP_AREA_CLASS+"'>Drop Area</div>");
+			$grid.append("<div class='"+CONTAINER_DROP_AREA_CLASS+"'>Drop Area</div>");
+			$grid.children(CSS_CONTAINER_DROP_AREA_CLASS).droppable({ 
+				accept: ".new-container",
+				drop: function( event, ui ) {
+					containerType =  $("select[name=container-type]").val();
+					$temp = buildContainer( 
+							[{ 	"type": containerType, 
+								"id" : "new",
+								"prolog": "prolog",
+								"epilog": "epilog" }] )
+							.insertBefore( $(this) );
+					
+					$grid.children().remove(CSS_CONTAINER_DROP_AREA_CLASS);
+					params = [ID, containerType, $temp.index()];
+					sendAjax("addContainer",params,
+					function(data){
+						console.log(data);
+						$temp.data("id", data.result.id);
+						$slots_wrapper = $temp.find(".slots-wrapper");
+						$.each( data.result.slots, function(index,value){
+							console.log(index+" "+value);
+							buildSlot([{"id" : value }]).appendTo( $slots_wrapper );
+						});
+					});
+				}
+			});
+		},
+		stop: function( event, ui ){
+			$grid.children().remove(CSS_CONTAINER_DROP_AREA_CLASS);
+		}
+	});
+	$grid.on("click",".c-tools > .c-tool", function(e){
+		$this = $(this);
+		$container = $this.parents(".container");
+		switch($this.attr("role")){
+			case "edit":
+				showContainerEditor($container);
+				break;
+			case "ok":
+				saveContainer($container);
+				break;
+			case "revert":
+				revertContainerChanges($container);
+				break;
+			case "trash":
+				deleteContainer($container);
+				break;
+			default:
+				console.log($this.attr("role"));
+		}
+	});
+	function deleteContainer($container){
+		params = [ID, $container.data("id")];
+		sendAjax("deleteContainer",params,function(data){
+			if(data.result != true){
+				alert("fehler beim Löschen!");
+				return;
+			}
+			$container.slideUp(300,function(){
+				$container.remove();
+			});
+		});
 	}
-	// Grid AJAX function ENDE
+	var revert_data;
+	var styles;
+	function showContainerEditor($container){
+		params = {
+					id:$container.data("id"), 
+					title: $container.find(".c-title").text(),
+					titleurl: $container.find(".c-titleurl").text(),
+					type: $container.data("type"),
+					prolog: $container.find(".c-prolog").html(),
+					epilog: $container.find(".c-epilog").html(),
+					readmore: $container.find(".c-readmore").text(),
+					readmoreurl: $container.find(".c-readmoreurl").text()
+				};
+				console.log(params);
+		revert_data = {};
+		revert_data = params;
+		$newContainer = buildContainerEditor(params).insertAfter( $container );
+		$newContainer.find(".slots-wrapper").replaceWith($container.find(".slots-wrapper"));
+		$container.remove();
+	}
+	function saveContainer($editContainer){
+		var style = $editContainer.find(".f-c-style").val();
+		if( style == "") style = null;
+		templateParams = {
+					id:$editContainer.data("id"), 
+					title: $editContainer.find("#f-c-title").val(),
+					titleurl: $editContainer.find("#f-c-titleurl").val(),
+					type: $editContainer.data("type"),
+					prolog: $editContainer.find("#f-c-prolog").val(),
+					epilog: $editContainer.find("#f-c-epilog").val(),
+					readmore: $editContainer.find("#f-c-readmore").val(),
+					readmoreurl: $editContainer.find("#f-c-readmoreurl").val(),
+					style: style
+				};
+		params =[ID, templateParams.id,{
+			style: templateParams.style,
+			title: templateParams.title,
+			titleurl: templateParams.titleurl,
+			readmore: templateParams.readmore,
+			readmoreurl: templateParams.readmoreurl,
+			prolog: templateParams.prolog,
+			epilog: templateParams.epilog,
+			style: style
+			}];
+		sendAjax("updateContainer",params,
+		function(data){
+			if(data.result == true){
+				$newContainer = buildContainer( templateParams ).insertAfter( $editContainer );
+				$newContainer.find(".slots-wrapper").replaceWith($editContainer.find(".slots-wrapper"));
+				$editContainer.remove();
+			} else {
+				alert("Konnte die Änderungen nicht speichern.");
+			}
+		});
+	}
+	function revertContainerChanges($container){
+		$oldContainer =	buildContainer(revert_data );
+		$oldContainer.find(".slots-wrapper").replaceWith($container.find(".slots-wrapper"));
+		$container.after($oldContainer);
+		$container.remove();
+	}
+	function buildContainer(templateParams){
+		return $.tmpl( "containerTemplate", templateParams );
+	}
+	function buildContainerEditor(templateParams){
+		return $.tmpl( "containerEditorTemplate", templateParams );
+	}
+	// ---------------------
+	// slot funktionen
+	// ----------------------
+	function buildSlot(templateParams){
+		return $.tmpl( "slotTemplate", templateParams );
+	}
+	
+	// --------------------
+	// Box Funktionen
+	// -------------------
+	function showBoxTools(){
+		sendAjax("searchBox",[""],function(data){
+			console.log(data);
+			$toolBoxList.empty();
+			$.each(data.result,function(index, value){
+				console.log(value);
+				$toolBoxList.append(buildBoxDraggable({id: value.id, title: value.title}));
+			});
+			$gridTools.children().hide();
+			$toolBox.show();
+			refreshBoxDraggables();
+		});
+	}
+	function refreshBoxSortable(){
+		$(".slot").sortable({
+			items: ".box",
+			connectWith: ".slot",
+			placeholder: "b-sort-placeholder",
+			forcePlaceholderSize: true,
+			helper: function(event, element){
+				return $("<div class='b-sort-helper'></div>");
+			},
+			start: function(e, ui){
+				ui.placeholder.height(ui.item.height());
+			},
+			cursorAt: { left: 30, top:30 }
+		});
+	}
+	function refreshBoxDraggables(){
+		console.log("refresh Dragger");
+		$(".box-dragger").draggable({ 
+			helper: "clone", 
+			zIndex: 199, 
+			//connectToSortable: GRID_SORTABLE,
+			start: function(event, ui){
+				$slots = $grid.find(".slot");
+				$slots.children(".box").before("<div class='"+BOX_DROP_AREA_CLASS+"'>Drop Box</div>");
+				$slots.append("<div class='"+BOX_DROP_AREA_CLASS+"'>Drop Box</div>");
+				$slots.children(CSS_BOX_DROP_AREA_CLASS).droppable({ 
+					accept: ".box-dragger",
+					hoverClass: "hover",
+					drop: function( event, ui ) {
+						$this_box = $(ui.draggable);
+						$this_drop = $(this);
+						$this_slot = $this_drop.parents(".slot");
+						$this_container = $this_slot.parents(".container");
+						$temp = buildBox( 
+								[{ 	id : $this_box.data("id"), content: $this_box.text() }] )
+								.insertBefore( $this_drop );
+						$slots.find(CSS_BOX_DROP_AREA_CLASS).remove();
+						
+						params = [ID, $this_container.data("id"),$this_slot.data("id"),$this_box.data("id"), $temp.index()];
+						console.log(params);
+						sendAjax("addBox",params,
+						function(data){
+							if(data.result == false){
+								alert("Konnte die Box nicht hinzufügen");
+								$this_box.remove();
+							}
+						});
+					}
+				});
+			},
+			stop: function( event, ui ){
+				$grid.find(CSS_BOX_DROP_AREA_CLASS).remove();
+			}
+		});
+	}
+	function buildBox(templateParams){
+		return $.tmpl( "boxTemplate", templateParams );
+	}
+	function buildBoxDraggable(templateParams){
+		return $.tmpl( "boxDraggableTemplate", templateParams );
+	}
+	
+	// --------------------
+	// GUI manipulation
+	// -------------------
 	var box_toggling = false;
 	function toggleBoxes(){
 		if(box_toggling) return;
 		toggling = true;
-		$(".box").slideToggle(200,function(){
+		$(".c-edit, .c-ok, .c-revert").toggle();
+		$(".box, .c-before, .c-after").slideToggle(200,function(){
 			box_toggling = false;
 		});
 	}
+	// --------------------
 	// Serverkommunikation
+	// -------------------
 	var SERVER = "http://emma-dev.ia-code.ws/grid/core/index.php?path=/ajax";
 	function sendAjax(method, params_array, success, error){
 		json = {};
 		json["method"] = method;
 		json["params"] = params_array;
+		if(error == null){
+			error = function(jqXHR, textStatus, error){
+				console.log(jqXHR);
+				console.log(textStatus);
+				console.log(error);
+			};
+		}
 		$.ajax({
 		   url: SERVER,
 		   dataType:"json",
--- js/templates.js
+++ js/templates.js
@@ -1,34 +1,34 @@
 
-var container_markup = '<div data-id="${id}" data-type="${type}" class="container ${type} clearfix">'+
-        	'<div class="tools">'+
-                '<span class="c-title">${type}</span>'+
-                '<span class="c-sort-handle"></span>'+
-                '<span class="c-trash"></span>'+
-				'<span class="c-edit"></span>'+
+var container_markup = '<div data-id="${id}" data-type="${type}" data-style="${style}" class="container ${type} clearfix">'+
+        	'<div class="c-tools">'+
+				
+                '<span role="sort-handle" class="c-sort-handle c-tool"></span>'+
+                '<span role="trash" class="c-trash c-tool"></span>'+
+				'<span role="edit" class="c-edit c-tool"></span>'+
             '</div>'+
-        	'<div class="c_before">'+
-				'<p>${title}</p>'+
-				'<p>${titleurl}</p>'+
-            	'<p>${prolog}</p>'+
+			'<p class="c-title">${title}</p>'+
+        	'<div class="c-before">'+
+				'<p class="c-titleurl">${titleurl}</p>'+
+            	'<p class="c-prolog">${prolog}</p>'+
             '</div>'+
-        	'<div class="slots-wrapper">{{if slots}}{{tmpl(slots) "slotTemplate"}}{{/if}}</div>'+
-            '<div class="c_after">'+
-            	'<p>${epilog}</p>'+
-				'<p>${readmore}</p>'+
-				'<p>${readmoreurl}</p>'+
-				'<p>${style}</p>'+
+        	'<div class="slots-wrapper clearfix">{{if slots}}{{tmpl(slots) "slotTemplate"}}{{/if}}</div>'+
+            '<div class="c-after">'+
+            	'<p class="c-epilog">${epilog}</p>'+
+				'<p class="c-readmore">${readmore}</p>'+
+				'<p class="c-readmoreurl">${readmoreurl}</p>'+
+				'<p class="c-style">${style}</p>'+
             '</div>'+
         '</div>';
 $.template( "containerTemplate", container_markup );
 
 var container_editor_markup = '<div data-id="${id}" data-type="${type}" class="container editor ${type} clearfix">'+
-        	'<div class="tools">'+
-                '<span class="c-title">${type}</span>'+
-                '<span class="c-ok"></span>'+
-                '<span class="c-sort-handle"></span>'+
-                '<span class="c-trash"></span>'+
+			'<div class="c-tools">'+
+                '<span role="ok" class="c-ok c-tool"></span>'+
+				'<span role="revert" class="c-revert c-tool"></span>'+
+                '<span role="sort-handle" class="c-sort-handle c-tool"></span>'+
+                '<span role="trash" class="c-trash c-tool"></span>'+
             '</div>'+
-        	'<div class="c_before">'+
+        	'<div class="c-before">'+
             	'<label for="f-c-title">Title:</label><br />'+
                 '<input name="f-c-title" id="f-c-title" value="${title}" /><br />'+
                 '<label for="f-c-titleurl">Titel URL:</label><br />'+
@@ -36,29 +36,27 @@ var container_editor_markup = '<div data-id="${id}" data-type="${type}" class="c
                 '<label for="f-c-prolog">Prolog:</label><br />'+
                 '<textarea name="f-c-prolog" id="f-c-prolog">${prolog}</textarea>'+
             '</div>'+
-        	'<div class="slots-wrapper">'+
+        	'<div class="slots-wrapper clearfix">'+
 				// slots hier anheften
             '</div>'+
-            '<div class="c_after">'+
+            '<div class="c-after">'+
                 '<label for="f-c-epilog">Epilog:</label><br />'+
         		'<textarea name="f-c-epilog" id="f-c-epilog">${epilog}</textarea> ' +              
             '</div>'+
             '<div class="clearfix">'+
             	'<fieldset>'+
-                    '<label for="f-c-readmore-text">Readmore Text:</label><br />'+
-                    '<input name="f-c-readmore-text" id="f-c-readmore-text" value="${readmore}" />'+
+                    '<label for="f-c-readmore">Readmore Text:</label><br />'+
+                    '<input name="f-c-readmore" id="f-c-readmore" value="${readmore}" />'+
                 '</fieldset>'+
                 '<fieldset>'+
-                    '<label for="f-c-readmore-link">Readmore URL:</label><br />'+
-                    '<input name="f-c-readmore-link" id="f-c-readmore-link" value="${readmoreurl}" />'+
+                    '<label for="f-c-readmoreurl">Readmore URL:</label><br />'+
+                    '<input name="f-c-readmoreurl" id="f-c-readmoreurl" value="${readmoreurl}" />'+
                 '</fieldset>'+
                 '<fieldset>'+
                     '<label for="f-c-style">Style</label><br />'+ 
                     '<select name="f-c-style" id="f-c-style">'+
                         '<option value="">ohne style</option>'+
-                        '<option value="style1">Style 1</option>'+
-                        '<option value="style2">Style 2</option>'+
-                        '<option value="style3">Style 3</option>'+
+                        '<option value="freeform">FreeForm</option>'+
                    '</select>'
                 '</fieldset>'+
             '</div>'+
@@ -68,5 +66,16 @@ $.template( "containerEditorTemplate", container_editor_markup );
 var slot_markup = '<div class="slot" data-id="${id}">{{if boxes}}{{tmpl(boxes) "boxTemplate"}}{{/if}}</div>';
 $.template( "slotTemplate", slot_markup );
 
-var box_markup = '<div class="box" data-id="${id}">${content}</div>';
+var box_markup = '<div class="box" data-id="${id}">'+
+						'<h3><a href="${titleurl}" class="box-title">${title}</a></h3>'+
+                        '<div class="prolog">{{html prolog}}</div>'+
+                        '<div class="content">${content}</div>'+
+                        '<div class="epilog">{{html epilog}}</div>'+
+                        '<p class="readmore"><a href="${readmoreurl}">${readmore}</a></p>'+
+				'</div>';
 $.template( "boxTemplate", box_markup );
+
+var box_draggable_markup = '<li class="box-dragger" data-id="${id}">'+
+							'<div class="handle"></div><div>${title}</div>'+
+							'</li>';
+$.template( "boxDraggableTemplate", box_draggable_markup );
