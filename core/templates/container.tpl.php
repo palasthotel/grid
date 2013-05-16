95f15a64f3fbff5e29545d6e08a064637fb6bb55
Edward Bock <me@edwardbock.de>
2013-05-16 13:38:25 +0200

    Drupal Grid JS
    migration fixes

--- core/templates/container.tpl.php
+++ core/templates/container.tpl.php
@@ -1,7 +1,19 @@
-<div class="region">
-	<a href="<?=$this->titleurl?>" class="region-title"><?=$this->title?></a>
-	<?=$this->prolog?>
+<div class="container_12 <?=$this->style?> clearfix">
+	<div class="clearfix">
+	<?
+	if ($this->title!=""){
+  ?>
+  <a href="<?=$this->titleurl?>" class="region-title"><?=$this->title?></a>
+	<?}?>
+  
+  <?=$this->prolog?>
+	</div>
 	<?=implode("", $slots)?>
+	<div class="clearfix">
+	<?
+	if ($this->readmore!=""){?>
 	<a href="<?=$this->readmoreurl?>"><?=$this->readmore?></a>
+	<?}?>
 	<?=$this->epilog?>
+	</div>
 </div>
--- js/templates.js
+++ js/templates.js
@@ -20,7 +20,7 @@ var container_markup = '<div data-id="${id}" data-type="${type}" data-style="${s
 				'<p class="c-style">${style}</p>'+
             '</div>'+
         '</div>';
-$.template( "containerTemplate", container_markup );
+jQuery.template( "containerTemplate", container_markup );
 
 var container_editor_markup = '<div data-id="${id}" data-type="${type}" class="container editor ${type} clearfix">'+
 			'<div class="c-tools">'+
@@ -31,32 +31,32 @@ var container_editor_markup = '<div data-id="${id}" data-type="${type}" class="c
 				'<span role="box-trash" class="c-box-trash"></span>'+
             '</div>'+
         	'<div class="c-before">'+
-            	'<label for="f-c-title">Title:</label><br />'+
-                '<input name="f-c-title" id="f-c-title" value="${title}" /><br />'+
-                '<label for="f-c-titleurl">Titel URL:</label><br />'+
-                '<input name="f-c-titleurl" id="f-c-titleurl" value="${titleurl}" /><br />'+
-                '<label for="f-c-prolog">Prolog:</label><br />'+
-                '<textarea name="f-c-prolog" id="f-c-prolog">${prolog}</textarea>'+
+            	'<label for="f-c-title">Title:</label>'+
+                '<input name="f-c-title" id="f-c-title" value="${title}" class="form-text" />'+
+                '<label for="f-c-titleurl">Titel URL:</label>'+
+                '<input name="f-c-titleurl" id="f-c-titleurl" value="${titleurl}" class="form-text" />'+
+                '<label for="f-c-prolog">Prolog:</label>'+
+                '<textarea name="f-c-prolog" id="f-c-prolog" class="form-textarea">${prolog}</textarea>'+
             '</div>'+
         	'<div class="slots-wrapper clearfix">'+
 				// slots hier anheften
             '</div>'+
             '<div class="c-after">'+
-                '<label for="f-c-epilog">Epilog:</label><br />'+
-        		'<textarea name="f-c-epilog" id="f-c-epilog">${epilog}</textarea> ' +              
+                '<label for="f-c-epilog">Epilog:</label>'+
+        		'<textarea name="f-c-epilog" id="f-c-epilog" class="form-textarea">${epilog}</textarea> ' +              
             '</div>'+
             '<div class="clearfix">'+
             	'<fieldset>'+
-                    '<label for="f-c-readmore">Readmore Text:</label><br />'+
-                    '<input name="f-c-readmore" id="f-c-readmore" value="${readmore}" />'+
+                    '<label for="f-c-readmore">Readmore Text:</label>'+
+                    '<input name="f-c-readmore" id="f-c-readmore" value="${readmore}" class="form-text" />'+
                 '</fieldset>'+
                 '<fieldset>'+
-                    '<label for="f-c-readmoreurl">Readmore URL:</label><br />'+
-                    '<input name="f-c-readmoreurl" id="f-c-readmoreurl" value="${readmoreurl}" />'+
+                    '<label for="f-c-readmoreurl">Readmore URL:</label>'+
+                    '<input name="f-c-readmoreurl" id="f-c-readmoreurl" value="${readmoreurl}" class="form-text" />'+
                 '</fieldset>'+
                 '<fieldset>'+
-                    '<label for="f-c-style">Style</label><br />'+ 
-                    '<select name="f-c-style" id="f-c-style">'+
+                    '<label for="f-c-style">Style</label>'+ 
+                    '<select name="f-c-style" id="f-c-style" class="form-select">'+
                         '<option value="">ohne style</option>'+
 						'{{if styles}}{{each styles}}'+
 							'<option {{if $value.slug == style }}selected="selected"{{/if}} value="${$value.slug}">${$value.title}</option>'+
@@ -65,7 +65,7 @@ var container_editor_markup = '<div data-id="${id}" data-type="${type}" class="c
                 '</fieldset>'+
             '</div>'+
         '</div>';
-$.template( "containerEditorTemplate", container_editor_markup );
+jQuery.template( "containerEditorTemplate", container_editor_markup );
 
 var slot_markup = '<div class="slot" data-id="${id}" data-style="${style}">'+
 						'<div class="style-changer">'+
@@ -74,7 +74,7 @@ var slot_markup = '<div class="slot" data-id="${id}" data-style="${style}">'+
 						'</div>'+
 						'<div class="boxes-wrapper">{{if boxes}}{{tmpl(boxes) "boxTemplate"}}{{/if}}</div>'+
 				'</div>';
-$.template( "slotTemplate", slot_markup );
+jQuery.template( "slotTemplate", slot_markup );
 
 var box_markup = '<div class="box" data-id="${id}" data-type="${type}">'+
 						'<h3><a href="${titleurl}" class="box-title">${title}</a></h3>'+
@@ -84,7 +84,7 @@ var box_markup = '<div class="box" data-id="${id}" data-type="${type}">'+
                         '<div class="epilog">{{html epilog}}</div>'+
                         '<p class="readmore"><a href="${readmoreurl}">${readmore}</a></p>'+
 				'</div>';
-$.template( "boxTemplate", box_markup );
+jQuery.template( "boxTemplate", box_markup );
 
 var box_draggable_markup = '<li class="box-dragger" data-id="${id}" data-type="${type}" data-index="${index}"'+
 							' data-titleurl="${titleurl}" data-readmore="${readmore}" data-readmoreurl="${readmoreurl}">'+
@@ -93,34 +93,39 @@ var box_draggable_markup = '<li class="box-dragger" data-id="${id}" data-type="$
 							'<div class="content">{{html html}}</div>'+
 							'<div class="epilog">{{html epilog}}</div>'+
 							'</li>';
-$.template( "boxDraggableTemplate", box_draggable_markup );
+jQuery.template( "boxDraggableTemplate", box_draggable_markup );
 
 var box_editor_markup = '<div class="box-editor" data-b-index="${b_index}" data-id="${box.id}" '+
 							'data-c-id="${c_id}" data-s-id="${s_id}" data-type="${box.type}">'+
 							'<fieldset>'+
-								'<legend>Title</legend><br />'+
-								'<input name="f-b-title" type="text" value="${box.title}" /> '+
-								'[URL:<input name="f-b-titleurl" type="text" value="${box.titleurl}" />]'+
+								'<legend>Title</legend>'+
+								'<label>Text</label>'+
+								'<input name="f-b-title" type="text" value="${box.title}" class="form-text" /> '+
+								'<label>URL</label>'+
+								'<input name="f-b-titleurl" type="text" value="${box.titleurl}" class="form-text" />'+
 							'</fieldset>'+
 							'<fieldset>'+
-								'<legend>Prolog:</legend><br />'+
-								'<textarea name="f-b-prolog">${box.prolog}</textarea>'+
+								'<legend>Prolog:</legend>'+
+								'<textarea name="f-b-prolog" class="form-textarea">${box.prolog}</textarea>'+
 							'</fieldset>'+
 							'<fieldset class="dynamic-fields">'+
+								'<legend>Dynamische Felder</legend>'+
 								// inputs for types added by script
 							'</fieldset>'+
 							'<fieldset>'+
-								'<legend>Epilog:</legend><br />'+
-								'<textarea name="f-b-epilog">${box.epilog}</textarea>'+
+								'<legend>Epilog:</legend>'+
+								'<textarea name="f-b-epilog"  class="form-textarea">${box.epilog}</textarea>'+
 							'</fieldset>'+
 							'<fieldset>'+
-								'<legend>Readmore</legend><br />'+
-								'<input name="f-b-readmore" type="text" value="${box.readmore}" /> '+
-								'[URL:<input name="f-b-readmoreurl" type="text" value="${box.readmoreurl}" />]'+
+								'<legend>Readmore</legend>'+
+								'<label>Text</label>'+
+								'<input name="f-b-readmore" type="text" value="${box.readmore}" class="form-text" /> '+
+								'<label>URL</label>'+
+								'<input name="f-b-readmoreurl" type="text" value="${box.readmoreurl}" class="form-text" />'+
 							'</fieldset>'+
 							'<fieldset>'+
-								'<label for="f-b-style">Style</label><br />'+ 
-								'<select name="f-b-style" id="f-b-style">'+
+								'<legend for="f-b-style">Style</legend>'+ 
+								'<select name="f-b-style" class="form-select"  id="f-b-style">'+
 									'<option value="">ohne style</option>'+
 									'{{if styles}}{{each styles}}'+
 										'<option {{if $value.slug == box.style }}selected="selected"{{/if}} value="${$value.slug}">'+
@@ -129,4 +134,4 @@ var box_editor_markup = '<div class="box-editor" data-b-index="${b_index}" data-
 								'</select>'
 							'</fieldset>'+
 						'</div>';
-$.template("boxEditorTemplate", box_editor_markup);
+jQuery.template("boxEditorTemplate", box_editor_markup);
