53492f959680229dbc7da56174583cab81e56d49
Edward <edward.bock@palasthotel.de>
2014-01-15 16:06:39 +0100

    added soundcloud box

--- core/classes/bootstrap.php
+++ core/classes/bootstrap.php
@@ -6,6 +6,7 @@ include 'grid_static_box.php';
 include 'grid_html_box.php';
 include 'grid_fb_like_box_box.php';
 include 'grid_video_box.php';
+include 'grid_soundcloud_box.php';
 include 'grid_list_box.php';
 include 'grid_reference_box.php';
 include 'grid_grid.php';
@@ -13,3 +14,4 @@ include 'grid_slot.php';
 include 'grid_container.php';
 include 'grid_db.php';
 include 'grid_ajaxendpoint.php';
+
