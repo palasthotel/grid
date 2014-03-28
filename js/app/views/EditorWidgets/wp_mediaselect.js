8abf9070baef4a65effe547dd249cbcd8397af5d
Edward <edward.bock@palasthotel.de>
2014-03-28 12:21:59 +0100

    mediaselect working

--- js/app/views/EditorWidgets/wp_mediaselect.js
+++ js/app/views/EditorWidgets/wp_mediaselect.js
@@ -16,26 +16,25 @@ boxEditorControls['wp-mediaselect']=GridBackbone.View.extend({
 
         this.$el.append(this.$upload_btn);
 
-        this.$input = $("<input />")
-                    .attr('value', JSON.stringify(c_val))
+        this.$input = jQuery("<input />")
+                    .attr('value', c_val)
                     .attr('type', 'hidden')
                     .addClass('dynamic-value form-json')
                     .attr("data-path",this.model.parentpath+element.key)
                     .attr('data-key', element.key);
         this.$el.append(this.$input);
+        
+        this.$select_image_size = jQuery("<select class='image-sizes'></select>");
+        this.$el.append(this.$select_image_size);
 
-        return this;
-                        $select_image_size = $("<select class='image-sizes'></select>");
-                        $dynamic_field.append($select_image_size);
-                        wp_buildImageSizeSelect(c_val, $input);
-                        $select_image_size.on("change",function(){
-                            $this = $(this);
-                            json = JSON.parse($this.siblings('.dynamic-value').val());
-                            json.size = $this.val();
-                            $this.siblings('.dynamic-value').val(JSON.stringify(json));
-                        });
+        this.$select_image_size.on("change",function(){
+            $this = jQuery(this);
+            json = JSON.parse($this.siblings('.dynamic-value').val());
+            json.size = $this.val();
+            $this.siblings('.dynamic-value').val(JSON.stringify(json));
+        });
 
-
+        this.buildImageSizeSelect();
 
         return this;
     },
@@ -54,7 +53,6 @@ boxEditorControls['wp-mediaselect']=GridBackbone.View.extend({
             selection.add( attachment ? [ attachment ] : [] );                          
         });
         frame.on('close',function() {
-            console.log("close");
             // get selections and save to hidden input plus other AJAX stuff etc.
             var selection = frame.state().get('selection');
             jQuery.each(frame.state().get('selection')._byId, function(id, val) {
@@ -63,37 +61,37 @@ boxEditorControls['wp-mediaselect']=GridBackbone.View.extend({
                     "size": "full",
                     "sizes": val.attributes.sizes
                 };
-                console.log(r_json);
                 self.$input.val(JSON.stringify(r_json));
-                //wp_buildImageSizeSelect(r_json, $button.siblings('.dynamic-value'));
+                self.buildImageSizeSelect(r_json, self.$input.val());
                 return false;
             });
         });
         frame.open();
         return false;
     },
-    // buildImageSizeSelect: function(){
-    //     var $dynamic_input = this.$input;
-    //     var json = JSON.parse(this.$input.val());
-    //     if(typeof json.sizes != "object"){
-    //         this.$select_image_size.hide();
-    //         return;
-    //     }
-    //     $.each(json.sizes, function(index, size) {
-    //          selected = "";
-    //          if(json.size == index){
-    //             selected = "selected='selected'";
-    //          }
-    //          jQuery("<option "+selected+" >"+index+"</option>").
-    //          attr('value', index).appendTo($select_image_size);
-    //     });
-    //     if($select_image_size.children().length > 0){ 
-    //         $select_image_size.show();
-    //     } else {
-    //         $select_image_size.hide();
-    //     }
-    // },
+    buildImageSizeSelect: function(){
+        var self = this;
+        var $dynamic_input = this.$input;
+        var json = JSON.parse(this.$input.val());
+        if(typeof json.sizes != "object"){
+            this.$select_image_size.hide();
+            return;
+        }
+        jQuery.each(json.sizes, function(index, size) {
+             selected = "";
+             if(json.size == index){
+                selected = "selected='selected'";
+             }
+             jQuery("<option "+selected+" >"+index+"</option>").
+             attr('value', index).appendTo(self.$select_image_size);
+        });
+        if(this.$select_image_size.children().length > 0){ 
+            this.$select_image_size.show();
+        } else {
+            this.$select_image_size.hide();
+        }
+    },
     fetchValue:function(){
-        return 42;
+        return this.$input.val();
     }
 });
