b91272bf7759adb799c50ca5fd5b9632b0f94a2c
Enno Welbers <enno.welbers@googlemail.com>
2014-03-28 14:05:33 +0100

    small update

--- grid.php
+++ grid.php
@@ -1,1 +1,34 @@
 <?php
+require("./classes/bootstrap.php");
+
+public class grid_library
+{
+	public function __construct()
+	{
+		
+	}
+	
+	//Gets an array of JS files to include on editor view
+	public function getJS()
+	{
+		
+	}
+	
+	//Gets an array of CSS files to include on editor view
+	public function getCSS()
+	{
+		
+	}
+	
+	//renders the CKEditor configuration
+	public function renderCKEditorConfig($styles,$formats)
+	{
+		
+	}
+	
+	//returns the needed editor HTML.
+	public function renderEditorHTML($path_to_ckeditor_config,)
+	{
+		
+	}
+}
