bc60b3e3d254631548c5ce4765fe75ac7ab3765c
Edward <edward.bock@palasthotel.de>
2014-04-08 17:11:42 +0200

    #282 checkbox widget

--- js/app/views/EditorWidgets/checkbox.js
+++ js/app/views/EditorWidgets/checkbox.js
@@ -5,14 +5,12 @@ boxEditorControls['checkbox']=GridBackbone.View.extend({
     render:function(){
         var value=this.model.container[this.model.structure.key];
         var checked='';
-        if(value)
-            checked='checked=checked';
-        html="<label>"+this.model.structure.label+"</label>";
-        html="<input type='checkbox' "+checked+"</input>";
-        jQuery(this.$el).html(html);
+        if(value) checked='checked=checked';
+        html="<label><input type='checkbox' "+checked+" /> "+this.model.structure.label+"</label>";
+        this.$el.html(html);
         return this;
     },
     fetchValue:function(){
-        return jQuery(this.$el).is(":checked");
+        return this.$el.find("input[type=checkbox]").is(":checked");
     }
 });
