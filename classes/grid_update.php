048a12be090989fd00931747b6cdd3f88576632a
Edward <edward.bock@palasthotel.de>
2014-04-17 12:20:46 +0200

    #299 more attributes set with update_2

--- classes/grid_update.php
+++ classes/grid_update.php
@@ -73,4 +73,58 @@ class grid_update
 		db_query("create table {grid_schema} (propkey varchar(255),value varchar(255));");
 		db_query("insert into {grid_schema} (propkey) values ('schema_version')");
 	}
+
+	public function update_2(){
+		db_query("ALTER TABLE {grid_container_type} ADD dimension varchar(255) DEFAULT NULL AFTER `type`;");
+		db_query("ALTER TABLE {grid_container_type} ADD space_to_right varchar(255) DEFAULT NULL AFTER `dimension`;");
+		db_query("ALTER TABLE {grid_container_type} ADD space_to_left varchar(255) DEFAULT NULL AFTER `dimension`;");
+
+		$result = db_query("SELECT * FROM {grid_container_type};");
+
+		foreach ($result as $record) {
+			$type = split("-", $recored->type);
+			for($i = 1; $i < count($type); $i++){
+				
+			}
+		}
+
+		// c-12 c-18
+		db_query("insert into {grid_container_type} (type, dimension, numslots) values ('c','1d1',1);");
+
+		// c-4-4-4 c-6-6-6
+		db_query("insert into {grid_container_type} (type, dimension, numslots) values ('c','1d3-1d3-1d3',3);");
+
+		// c-8-4 c-4-8 c-6-12 c-12-6
+		db_query("insert into {grid_container_type} (type, dimension, numslots) values ('c','1d3-2d3',2);");
+		db_query("insert into {grid_container_type} (type, dimension, numslots) values ('c','2d3-1d3',2);");
+
+		// c-2-2-2-2-2-2 c-3-3-3-3-3-3
+		db_query("insert into {grid_container_type} (type, dimension, numslots) values ('c','1d6-1d6-1d6-1d6-1d6-1d6',6);");
+
+		//c-3-3-3-3
+		db_query("insert into {grid_container_type} (type, dimension, numslots) values ('c','1d4-1d4-1d4-1d4',4);");
+
+		// c-6-6 c-8-8
+		db_query("insert into {grid_container_type} (type, dimension, numslots) values ('c','1d2-1d2',2);");
+
+		// sc-4 sc-6
+		db_query("insert into {grid_container_type} (type, dimension, numslots) values ('c','1d1',1);");
+
+		// c-0-4-4 c-0-6-6 && c-4-4-0 c-6-6-0
+		db_query("insert into {grid_container_type} (type, dimension, space_to_left, numslots) values ('c','1d3-1d3', '1d3',2);");
+		db_query("insert into {grid_container_type} (type, dimension, space_to_right, numslots) values ('c','1d3-1d3', '1d3',2);");
+
+		// c-0-8 c-0-12 && c-8-0 c-12-0
+		db_query("insert into {grid_container_type} (type, dimension, space_to_left, numslots) values ('c','2d3', '1d3',1);");
+		db_query("insert into {grid_container_type} (type, dimension, space_to_right, numslots) values ('c','2d3', '1d3',1);");
+
+		// s-4-0 s-0-4 && s-6-0 s-0-6
+		db_query("insert into {grid_container_type} (type, dimension, space_to_left, numslots) values ('s','1d3', '2d3',1);");
+		db_query("insert into {grid_container_type} (type, dimension, space_to_right, numslots) values ('s','1d3', '2d3',1);");
+
+		// I-0
+		db_query("insert into {grid_container_type} (type, numslots) values ('i','0', 0);");
+
+	}
+
 }
