2262b957864702e8a3068110f981daa8bef1c0e6
Kim <info@greatestview.de>
2014-07-04 13:44:55 +0200

    added support for IE9 and below + some enhancements

--- classes/grid_soundcloud_box.php
+++ classes/grid_soundcloud_box.php
@@ -1,60 +1,100 @@
 <?php
 
 class grid_soundcloud_box extends grid_static_base_box {
-	
-	public function type()
-	{
-		return 'soundcloud';
-	}
 
-	public function __construct()
-	{
-		$this->content=new Stdclass();
-		$this->content->url= "";
-		$this->content->color = "";
-	}
+    public function type()
+    {
+        return 'soundcloud';
+    }
 
-	public function build($editmode) {
-		if($editmode)
-		{
-			return t("Soundcloud").": ".$this->content->url;
-		}
-		else
-		{
+    public function __construct()
+    {
+        $this->content=new Stdclass();
+        $this->content->url= "";
+        $this->content->color = "";
+    }
 
-			$url = "http://soundcloud.com/oembed?";
-			$url.= "url=".$this->content->url."&format=json&maxheight=300";
-			if($this->content->color != "" && strlen($this->content->color) == 6) $url.= "&color=".$this->content->color;
+    public function build($editmode) {
+        if($editmode)
+        {
+            return t("Soundcloud").": ".$this->content->url;
+        }
+        else
+        {
+            // KM added support for IE9 and below
+            $url = $this->content->url;
 
+            if (empty($this->content->url)) {
+                return "<p>" . t("Please provide a Soundcloud URL") . "</p>";
+            }
 
-			$curl = curl_init($url);
-		    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		    curl_setopt($curl, CURLOPT_TIMEOUT, 30);
-		    $return = curl_exec($curl);
-		    curl_close($curl);
+            $iframe_maxheight = 200;
+            $oembed_maxheight = 81;
+            // soundcloud playlist needs more vertical space
+            if (strpos($url, '/sets/') !== false) {
+              $iframe_maxheight = 300;
+              $oembed_maxheight = 300;
+            }
 
-		    if($return== " " || !$return || $this->content->url == ""){
-				return "<p>".t("no track found")."</p>";
-			}
+            $query_iframe = "&format=json&maxheight=$iframe_maxheight&auto_play=false&url=$url";
+            $query_oembed = "&format=json&maxheight=$oembed_maxheight&auto_play=false&url=$url&iframe=false";
 
-			$json = json_decode($return);
-			return $json->html;
-		}
-	}
-	
-	public function contentStructure () {
-		return array(
-			array(
-				'key'=>'url',
-				'label'=>t('URL'),
-				'type'=>'text'
-			),
-			array(
-				'key'=>'color',
-				'label'=>t('Hex Color #[...] (optional)'),
-				'type'=>'text'
-			),
-		);
-	}
+            $request_url_iframe = "http://soundcloud.com/oembed?url=" . $this->content->url . $query_iframe;
+            $request_url_oembed = "http://soundcloud.com/oembed?url=" . $this->content->url . $query_oembed;
+
+            if ($this->content->color != "" && strlen($this->content->color) == 6) {
+                $request_url_iframe .=  "&color=" . $this->content->color;
+                $request_url_oembed .=  "&color=" . $this->content->color;
+            }
+
+            // first request with iframe format:
+            $curl_iframe = curl_init($request_url_iframe);
+            curl_setopt($curl_iframe, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl_iframe, CURLOPT_TIMEOUT, 30);
+            $return_iframe = curl_exec($curl_iframe);
+            curl_close($curl_iframe);
+
+            // second request without iframe format for ie9 and below
+            $curl_oembed = curl_init($request_url_oembed);
+            curl_setopt($curl_oembed, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl_oembed, CURLOPT_TIMEOUT, 30);
+            $return_oembed = curl_exec($curl_oembed);
+            curl_close($curl_oembed);
+
+            if (empty($return_iframe) || empty($return_oembed)) {
+                return "<p>" . t("Please provide a valid Soundcloud URL") . "</p>";
+            }
+
+            $json_iframe = json_decode($return_iframe);
+            $json_oembed = json_decode($return_oembed);
+
+            $html_iframe = $json_iframe->html;
+            $html_oembed = $json_oembed->html;
+
+            return <<<EOT
+<!--[if gt IE 9]><!-->
+$html_iframe
+<!--<![endif]-->
+<!--[if lte IE 9]>
+$html_oembed
+<![endif]-->
+EOT;
+        }
+    }
+
+    public function contentStructure () {
+        return array(
+            array(
+                'key'=>'url',
+                'label'=>t('URL'),
+                'type'=>'text'
+            ),
+            array(
+                'key'=>'color',
+                'label'=>t('Hex Color #[...] (optional)'),
+                'type'=>'text'
+            ),
+        );
+    }
 
 }
