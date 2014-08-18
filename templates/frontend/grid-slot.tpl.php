d354f7bff53d9d8a02b3ccb8e58ec2e2c617574a
jdwestermann <daniel.westermann@web.de>
2014-08-18 14:53:01 +0200

    Update grid-slot.tpl.php

--- templates/frontend/grid-slot.tpl.php
+++ templates/frontend/grid-slot.tpl.php
@@ -1,6 +1,6 @@
 
 <div class="grid-slot grid-slot-<?php echo $this->dimension; echo ($this->style)? " ".$this->style." ":" "; echo implode($this->classes," ");?>">
 	<div class="grid-boxes-wrapper">
-		<?=implode("", $boxes)?>
+		<?php echo implode("", $boxes)?>
 	</div>
 </div>
