b9e4405cb3f10d7a9fc368060ac69ded3de7653b
Edward <edward.bock@palasthotel.de>
2015-01-05 14:05:25 +0100

    comment

--- classes/grid_error_box.php
+++ classes/grid_error_box.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * @author Palasthotel <rezeption@palasthotel.de>
+ * @copyright Copyright (c) 2014, Palasthotel
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GPLv2
+ * @package Palasthotel\Grid
+ */
+/**
+* Error-Box that displays Error if there are some when loading grid box classes
+*/
 class grid_error_box extends grid_box {
 
 	/**
