a7840ceafca41e02ecbef2e8d7d5abb6f54874d9
Edward <edward.bock@palasthotel.de>
2015-03-06 16:12:45 +0100

    websocket works

--- js/app/GridAsync.js
+++ js/app/GridAsync.js
@@ -1,17 +1,37 @@
 function GridAsync(){
+	this.domain = window.location.host;
+	this.path = window.location.pathname;
+	this.author = document.author;
 	this.socket = io('http://grid-dev.palasthotel.de:61000');
-	var self = this;
-	this.on("connect", this.onConnect);
+
+	this.on("connect", "connect");
+	this.on("users.list", "users_list");
+	this.on("users.joined", "users_joined");
+	this.on("users.left", "users_left");
 }
 GridAsync.prototype.on = function(e, f){
-	this.socket.on(e, function(data){ f(data);  });
+	var self = this;
+	this.socket.on(e, function(data){ 
+		self[f](data);
+	});
+		
 }
 
-GridAsync.prototype.onConnect = function(data){
+GridAsync.prototype.connect = function(data){
 	console.log("connected");
+	this.join();
+};
+GridAsync.prototype.join = function(){
+	this.socket.emit("users.join", {domain: this.domain, path: this.path, author: this.author});
+}
+GridAsync.prototype.users_list = function(data){
+	console.log(data);
+};
+GridAsync.prototype.users_joined = function(data){
+	console.log(data);
+};
+GridAsync.prototype.users_left = function(data){
 	console.log(data);
 };
 
-GridAsync.prototype.join = function(){
-	this.socket.emit("users.join", {domain: "local-grid-drupal-dev", path: "grid/"+GRID.ID, username: "Edward"});
-}
+
