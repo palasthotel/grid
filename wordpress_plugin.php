1392faedfd0da96199dd66a4770cf0f9e9599fd9
Edward <edward.bock@palasthotel.de>
2015-05-06 10:53:14 +0200

    little forgettenings

--- wordpress_plugin.php
+++ wordpress_plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Grid
  * Plugin URI: https://github.com/palasthotel/grid/
  * Description: Helps layouting pages with containerist.
- * Version: 1.3.3
+ * Version: 1.3.4
  * Author: Palasthotel <rezeption@palasthotel.de> (in person: Benjamin Birkenhake, Edward Bock, Enno Welbers)
  * Author URI: http://www.palasthotel.de
  * Requires at least: 4.0
