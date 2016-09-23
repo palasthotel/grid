273e4050f43a881c055f937df5206a24d441eeee
EdwardBock <edward.bock@palasthotel.de>
2016-09-23 12:52:04 +0200

    multiselect widget

--- js/app/views/EditorWidgets/multi-autocomplete.js
+++ js/app/views/EditorWidgets/multi-autocomplete.js
@@ -23,8 +23,9 @@ boxEditorControls['multi-autocomplete']=GridBackbone.View.extend({
         {
             fetch=true;
         }
+        
         html+="<div class='"+classes+"'><input type=text class='form-text autocomplete i-autocomplete' />";
-        html+="<div class=loading rotate'></div>";
+	    html+="<div class='loading'></div>";
         html+="<ul class='suggestion-list'></ul>";
         
 
@@ -39,19 +40,40 @@ boxEditorControls['multi-autocomplete']=GridBackbone.View.extend({
 
         if(fetch)
         {
-            var key=this.model.container[this.model.structure.key];
-            this.$el.find(".autocomplete-multiselect-item").data("key");
-            var box=this.model.box;
             var self=this;
-            $.each(this.model.container[this.model.structure.key], function(index, key) 
-            {
-                GridAjax("typeAheadGetText",[box.getGrid().get("id"),box.getContainer().get("id"),box.getSlot().get("id"),box.getIndex(),self.model.parentpath+self.model.structure.key,key],
-                {
-                    success_fn:function(data)
+            var box=this.model.box;
+            var content = this.model.container[this.model.structure.key];
+            
+            if(typeof content === typeof undefined){
+                content = [];
+            } else if( typeof content !== typeof []){
+                // legacy: before multi autoselect support
+                content = [content];
+            }
+	        
+            jQuery.each(content, function(index, key){
+	            self.$el.find(".loading").addClass("go");
+                GridAjax(
+                    "typeAheadGetText",
+                    [
+                        box.getGrid().get("id"),
+                        box.getContainer().get("id"),box.getSlot().get("id"),
+                        box.getIndex(),
+                        self.model.parentpath+self.model.structure.key,
+                        key
+                    ],
                     {
-                        self.$el.find(".autocomplete-wrapper").append("<div class='autocomplete-multiselect-item' data-key='"+key+"'><span>"+data.result+"</span><span class='icon icon-cancel'></span></div>");
+                        success_fn:function(data) {
+	                        self.$el.find(".loading").removeClass("go");
+                            self.$el
+                                .find(".autocomplete-wrapper")
+                                .append("<div class='autocomplete-multiselect-item' data-key='"+key+"'>"+
+                                    "<span>"+data.result+"</span>"+
+                                    "<span class='icon icon-cancel'></span>"+
+                                    "</div>");
                     }
                 });
+                
             });
         }
         
@@ -72,27 +94,59 @@ boxEditorControls['multi-autocomplete']=GridBackbone.View.extend({
         }
         else
         {
-            if(this.$el.find("input.i-autocomplete").val()==this.old_search_string)return;
             var self=this;
             var search=this.$el.find("input.i-autocomplete").val();
-            this.$el.find(".loading").show();
-            var box=this.model.box;
-            GridAjax("typeAheadSearch",[box.getGrid().get("id"),box.getContainer().get("id"),box.getSlot().get("id"),box.getIndex(),this.model.parentpath+this.model.structure.key,search],{
-                success_fn:function(data){
-                    self.old_search_string=search;
-                    self.$el.find(".suggestion-list").empty();
-                    _.each(data.result,function(elem){
-                        self.$el.find(".suggestion-list").append(jQuery("<li>"+elem.value+"</li>").attr("data-key",elem.key));
-                    });
-                    self.$el.find(".loading").hide();
-                }
-            });
+            
+            /**
+             * dont execute same query twice
+             */
+            if( this.$el.find("input.i-autocomplete").val() == this.old_search_string ) return;
+            self.old_search_string=search;
+            
+            /**
+             * animate loading
+             */
+            this.$el.find(".loading").addClass("go");
+    
+            /**
+             * start search timeout
+             */
+            clearTimeout(this.searching);
+            this.searching = setTimeout(function(){
+                self.doSearch(search);
+            }, 1000);
+            
         }
+    },
+    doSearch: function(search){
+        var self = this;
+        var box=this.model.box;
+        
+        GridAjax(
+            "typeAheadSearch",
+            [
+                box.getGrid().get("id"),
+                box.getContainer().get("id"),
+                box.getSlot().get("id"),
+                box.getIndex(),
+                this.model.parentpath+this.model.structure.key,
+                search
+            ],
+            {
+            success_fn:function(data){
+                self.$el.find(".suggestion-list").empty();
+                _.each(data.result,function(elem){
+                    self.$el.find(".suggestion-list").append(jQuery("<li>"+elem.value+"</li>").attr("data-key",elem.key));
+                });
+                self.$el.find(".loading").removeClass("go");
+            }
+        });
+        
     },
     selectItem:function($item){
         var key=$item.data("key");
         var value=$item.text();
-        if($(".autocomplete-multiselect-item[data-key='"+key+"']").length) 
+        if(jQuery(".autocomplete-multiselect-item[data-key='"+key+"']").length)
                 {
                     this.$el.find(".suggestion-list").empty();
                     this.$el.find("input.i-autocomplete").removeAttr('value');
