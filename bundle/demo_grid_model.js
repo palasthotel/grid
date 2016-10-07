ebb3ffbbb41de445f947e21f7cb7cd971f26a28c
EdwardBock <edward.bock@palasthotel.de>
2016-10-07 09:58:20 +0200

    methods display

--- bundle/demo_grid_model.js
+++ bundle/demo_grid_model.js
@@ -54,27 +54,47 @@
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	window.GridBackend = new _backend2.default("http://grid.local:8080/wp-admin/admin.php?noheader=1&page=grid_ajax");
-	GridBackend.execute("grid.test", "allMethods", [], function (data) {
-	  console.log(data);
-	});
-	
 	/**
 	 * grid dummy
 	 */
 	var grid = __webpack_require__(184);
-	
 	window.GRID = new _grid.Grid(grid);
-	
 	var demo = document.getElementById("demo");
+	
+	/**
+	 * methods
+	 */
+	var ajaxurl = "http://grid.local:8080/wp-admin/admin-ajax.php?action=grid_ajax";
+	window.GridBackend = new _backend2.default(ajaxurl);
+	GridBackend.execute("grid.test", "allMethods", [], function (data) {
+		console.log(data);
+		demo.innerHTML += "<h1>Ajax methods</h1>";
+		demo.innerHTML += "<p>Ajax URL: " + ajaxurl + "</p>";
+		for (var component_key in data.result) {
+			var component = data.result[component_key];
+			demo.innerHTML += "<h2>" + component_key + "</h2>";
+	
+			var component_html = "<p>Class: " + component.class + "<br />" + "File: " + component.file + "</p>";
+	
+			var methods_html = "";
+	
+			methods_html += "<ul style='padding: 0 30px;' >";
+			for (var i = 0; i < component.methods.length; i++) {
+				methods_html += "<li>" + component.methods[i] + "</li>";
+			}
+			methods_html += "</ul>";
+	
+			demo.innerHTML += methods_html;
+		}
+	});
 	
 	/**
 	 * check it
 	 */
 	GRID.container.forEach(function (container) {
-	  console.log("the container", container);
-	  container.grid = GRID;
-	  demo.innerHTML += "<p>" + container.get("title") + "</p>";
+		console.log("the container", container);
+		container.grid = GRID;
+		demo.innerHTML += "<p>" + container.get("title") + "</p>";
 	});
 
 /***/ },
--- demo/grid-model.js
+++ demo/grid-model.js
@@ -5,19 +5,41 @@ import {Grid} from '../src/model/grid.js';
 import Backend from '../src/connection/backend.js';
 
 
-window.GridBackend = new Backend("http://grid.local:8080/wp-admin/admin.php?noheader=1&page=grid_ajax");
-GridBackend.execute("grid.test","allMethods",[],function(data){
-	console.log(data)
-});
-
 /**
  * grid dummy
  */
 const grid = require('./dummy-data/grid');
-
 window.GRID = new Grid(grid);
-
 var demo = document.getElementById("demo");
+
+/**
+ * methods
+ */
+const ajaxurl = "http://grid.local:8080/wp-admin/admin-ajax.php?action=grid_ajax";
+window.GridBackend = new Backend(ajaxurl);
+GridBackend.execute("grid.test","allMethods",[],function(data){
+	console.log(data);
+	demo.innerHTML+= "<h1>Ajax methods</h1>";
+	demo.innerHTML+="<p>Ajax URL: "+ajaxurl+"</p>";
+	for(let component_key in data.result){
+		let component = data.result[component_key];
+		demo.innerHTML+="<h2>"+component_key+"</h2>";
+		
+		let component_html="<p>Class: "+component.class+"<br />"+"File: "+component.file+"</p>";
+		
+		let methods_html = "";
+		
+		methods_html+="<ul style='padding: 0 30px;' >";
+		for(let i = 0; i < component.methods.length; i++){
+			methods_html+="<li>"+component.methods[i]+"</li>";
+		}
+		methods_html+="</ul>";
+		
+		demo.innerHTML+=methods_html;
+	}
+});
+
+
 
 /**
  * check it
