94ec41c865b53479f41d439f99a2c79b4b634236
EdwardBock <edward.bock@palasthotel.de>
2016-12-14 15:36:44 +0100

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,47 @@
 
-module.exports = {
-	entry: require('./webpack.entries.js'),
+let config = {
+	entry: {
+		/**
+		 * all plugin information
+		 */
+		plugins: './src/plugins.js',
+		/**
+		 * script for the grid
+		 */
+		the_grid: './src/the-grid.js',
+		/**
+		 * demo of a whole grid
+		 */
+		demo_the_grid: './demo/the-grid.js',
+		/**
+		 * demo of only the grid content part
+		 */
+		demo_grid: './demo/grid.js',
+		/**
+		 * demo of grid model
+		 */
+		demo_grid_model: './demo/grid-model.js',
+		/**
+		 * demo of box editor
+		 */
+		demo_box_editor: './demo/box-editor.js',
+		/**
+		 * container drag and drop demo
+		 */
+		demo_container_dnd: './demo/container-dnd.js',
+		/**
+		 * demo of styles editor
+		 */
+		demo_styles_editor: './demo/styles-editor.js',
+		/**
+		 * demo of container factory
+		 */
+		demo_container_factory: './demo/container-factory.js',
+		/**
+		 * demo of tabview with container and boxes
+		 */
+		demo_tab_view: './demo/tab-view.js',
+	},
 	output: {
 		path: './bundle',
 		filename: '[name].js',
@@ -20,3 +61,10 @@ module.exports = {
 	},
 	watch: true,
 };
+
+if(process.env.NODE_ENV == "production"){
+	config.output.filename = "[name].min.js";
+	config.output.sourceMapFilename = "[name].min.map";
+}
+
+module.exports = config;
