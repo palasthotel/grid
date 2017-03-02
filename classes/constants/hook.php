95f969197c08773e42c2145ef8f67dc32535232f
Edward <me@edwardbock.de>
2017-03-02 10:01:35 +0100

    Update hook.php
    Hooks from master branch

--- classes/constants/hook.php
+++ classes/constants/hook.php
@@ -12,6 +12,12 @@ class Hook{
 	const WILL_RENDER_GRID = "will_render_grid";
 	const DID_RENDER_GRID = "did_render_grid";
 	
+	const WILL_RENDER_CONTAINER = "will_render_container";
+	const DID_RENDER_CONTAINER = "did_render_container";
+	
+	const WILL_RENDER_SLOT = "will_render_slot";
+	const DID_RENDER_SLOT = "did_render_slot";
+	
 	const WILL_RENDER_BOX = "will_render_box";
 	const DID_RENDER_BOX = "did_render_box";
 	
