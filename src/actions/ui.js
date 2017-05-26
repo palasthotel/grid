7f6b4e31d70069bec422da3f558f808819983066
EdwardBock <edward.bock@palasthotel.de>
2017-05-26 15:46:46 +0200

    gui state

--- src/actions/ui.js
+++ src/actions/ui.js
@@ -2,13 +2,39 @@
 
 
 import {
+	GRID_UI_STATE,
 	GRID_LOADING,
 	GRID_CONTAINER_EDIT,
 	GRID_BOX_EDIT,
+	GRID_CONTAINER_IN_PLACE_DIALOG,
 } from './types';
+
+/**
+ * genereal ui state change
+ * @param key
+ * @param value
+ * @return {{type: string, payload: {key: string, value: * }}}
+ */
+export function actionUiStateChange(key, value){
+	return {type: GRID_UI_STATE, payload:{ key, value }};
+}
+
 
 export function actionSetGridLoading(is_loading) {
 	return {type: GRID_LOADING, payload: { is_loading }};
+}
+
+export function actionContainerHideInPlaceDialog(){
+	return actionContainerShowInPlaceDialog();
+}
+
+/**
+ *
+ * @param args
+ * @return {{type, payload: { index:, others} }}
+ */
+export function actionContainerShowInPlaceDialog( args ){
+	return {type:GRID_CONTAINER_IN_PLACE_DIALOG, payload: { ...args } }
 }
 
 export function actionEditGridContainerClose(){
--- src/reducer/update-ui.js
+++ src/reducer/update-ui.js
@@ -1,12 +1,23 @@
 
 import {
+	GRID_UI_STATE,
 	GRID_LOADING,
 	GRID_BOX_EDIT,
 	GRID_CONTAINER_EDIT,
+	GRID_CONTAINER_IN_PLACE_DIALOG,
+
 } from '../actions/types';
 
 export default function updateUI(state, action){
 	switch (action.type) {
+
+		case GRID_UI_STATE:
+			const new_state = {
+				...state,
+			};
+			new_state[action.payload.key] = action.payload.value;
+			return new_state;
+
 		case GRID_LOADING:
 			return {
 				...state,
@@ -22,6 +33,18 @@ export default function updateUI(state, action){
 			return {
 				...state,
 				edit_box: action.payload.box,
+			}
+		case GRID_CONTAINER_IN_PLACE_DIALOG:
+
+			const container_dialog = {
+				...state.container_dialog,
+				...action.payload,
+				index: action.payload.index,
+			}
+
+			return {
+				...state,
+				container_dialog,
 			}
 		default:
 			return state;
