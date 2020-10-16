0deb4e7e322ce6a1c20bbd6b4072e9139ef63d5d
EdwardBock <edward.bock@palasthotel.de>
2020-10-16 15:48:18 +0200

    rename

--- bin/build-plugin.sh
+++ bin/build-plugin.sh
@@ -18,6 +18,8 @@ rm "$DEST_PATH/lib/grid/.gitignore"
 rm "$DEST_PATH/lib/grid/Butlerfile"
 rm "$DEST_PATH/lib/grid/composer.json"
 rm "$DEST_PATH/lib/grid/package.json"
+rm "$DEST_PATH/lib/grid/package-lock.json"
+rm "$DEST_PATH/lib/grid/node_modules"
 rm $DEST_PATH/lib/grid/webpack*
 rm -r "$DEST_PATH/lib/grid/src"
 rm -r "$DEST_PATH/lib/grid/scss"
@@ -29,5 +31,8 @@ zip -q -r "${PLUGIN_SLUG}.zip" "$PLUGIN_SLUG/"
 cd "$PROJECT_PATH" || exit
 mv "$BUILD_PATH/${PLUGIN_SLUG}.zip" "$PROJECT_PATH"
 echo "${PLUGIN_SLUG}.zip file generated!"
+
+echo "Cleanup build path..."
+rm -rf "$BUILD_PATH"
 
 echo "Build done!"
